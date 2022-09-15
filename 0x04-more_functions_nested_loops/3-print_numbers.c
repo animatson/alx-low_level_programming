a63113fb03c02d960b6b6c75bb2da7e273b2aaf2
aniseti <anicetysanya@gmail.com>
2022-09-15 05:10:44 -0700

    check this out yooh!

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - print number from 0 to 9
+ */
+void print_numbers(void)
+{
+	for (char i = '0' ; i < '10' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
