d763e4078701d2e5994266917e8b33cc9d966977
aniseti <anicetysanya@gmail.com>
2022-09-19 08:46:58 -0700

    put value

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+/**
+ * _puts - function to put a string
+ * @str: pass by ref
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int pos;
+
+	for (pos = 0 ; *(str + pos) != '\0' ; pos++)
+		_putchar(*(str + pos));
+}
