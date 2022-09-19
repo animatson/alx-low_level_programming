1478a36b600bf8773d2ac49ae5035b6c7dcd80d6
aniseti <anicetysanya@gmail.com>
2022-09-19 10:16:59 -0700

    up again

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a word
+ * @s: The string to be printed.
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len = len + 1;
+	len = len - 1;
+	for (; len >= 0 ; len--)
+		_putchar(s[len]);
+	_putchar('\n');
+}
