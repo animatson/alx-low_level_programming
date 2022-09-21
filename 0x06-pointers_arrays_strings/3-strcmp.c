d498ddc51713c0ac799a2bf753784773b1b827e7
aniseti <anicetysanya@gmail.com>
2022-09-21 13:23:35 -0700

    again

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,14 @@ int _strcmp(char *s1, char *s2)
 
 	for (; s1[i] != '\0' && s2[i] != '\0' ; i++)
 	{
-		if (s1[i] > s2[i])
-			return (15);
-		else if (s1[i] < s2[i])
-			return (-15)
+		int a, b;
+
+		a = s1[i];
+		b = s2[i];
+		if (a < b)
+			return (-15);
+		else if (a > b)
+			return  (15);
 	}
 	return (0);
 }
