e345ebeaea948421ea5416ae26aea46698011377
aniseti <anicetysanya@gmail.com>
2022-09-26 09:52:49 -0700

    only betty

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+/**
+ * _strspn - counting 
+ * @s: pass by ref
+ * @a: pass by ref
+ * Return: number of counts
+ */
+unsigned int _strspn(char *s, char *a)
+{
+	int i = 0, j = 0;
+	unsigned int count = 0;
+
+	for (; s[i] != '\0' ; i++)
+	{
+		for (; a[j] != '\0' ; j++)
+		{
+			if (s[i] == a[j])
+				++count;
+		}
+	}
+	return (count);
+}
