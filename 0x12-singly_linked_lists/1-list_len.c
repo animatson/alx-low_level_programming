9dbaedd6c23258db8cfb9414c81c14abfba432e6
aniseti <anicetysanya@gmail.com>
2022-10-20 12:35:31 -0700

    second fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,7 +11,7 @@ size_t list_len(const list_t *h)
 
         while (h)
         {
-                ++count;
+                count++;
                 h = h->next;
         }
         return (count);
